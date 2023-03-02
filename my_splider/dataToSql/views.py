b5a5363343b96dc1639b39306b604372e105359c
hj-long <1203725971@qq.com>
2023-03-03 01:12:39 +0800

    测试数据库使用成功

--- my_splider/dataToSql/views.py
+++ my_splider/dataToSql/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from my_splider.models import GoodsInfo
+import json
 
 
 # Create your views here.
+# 从数据库中读取数据
 def sql_data(request):
-    return HttpResponse('Hello World!')
+    # 从数据库中读取数据
+    goods_info = GoodsInfo.objects.get(id=1)
+    # goods = []
+    # for i in goods_info:
+    #     item = {
+    #         'id': i.id,
+    #         'title': i.title,
+    #         'price': i.price,
+    #         'sale_sum': i.sale_sum,
+    #         'link': i.link,
+    #         'detail': i.detail,
+    #     }
+    #     goods.append(item)
+    
+    # 将数据转换为json格式
+    # goods = json.dumps(goods, ensure_ascii=False)
+    # 传递数据到模板
+    return render(request, 'sql_data.html', {'goods': goods_info})
